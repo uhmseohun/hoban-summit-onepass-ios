9c28b17fcf6afd596efbf6da5be8a916217145ea
uhmtoto <uhmtotow@gmail.com>
2020-01-26 21:51:19 +0900

    Initialize project

--- hoban-summit-onepass-ios/ViewController.swift
+++ hoban-summit-onepass-ios/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreBluetooth
+import CoreLocation
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+    var localBeacon: CLBeaconRegion!
+    var beaconPeripheralData: NSDictionary!
+    var peripheralManager: CBPeripheralManager!
+
+    func initLocalBeacon () {
+        if localBeacon != nil {
+            stopLocalBeacon()
+        }
+
+        let localBeaconUUID = UUID(uuidString: "54573114-0102-3203-3425-201062190152")!
+        let localBeaconMajor: CLBeaconMajorValue = 123
+        let localBeaconMinor: CLBeaconMinorValue = 456
+
+        localBeacon = CLBeaconRegion(proximityUUID: localBeaconUUID, major: localBeaconMajor, minor: localBeaconMinor, identifier: "identifier")
+
+        beaconPeripheralData = localBeacon.peripheralData(withMeasuredPower: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
     }
 
+    func stopLocalBeacon () {
+        peripheralManager.stopAdvertising()
+        peripheralManager = nil
+        beaconPeripheralData = nil
+        localBeacon = nil
+    }
 
+    func peripheralManagerDidUpdateState (_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            peripheralManager.startAdvertising(beaconPeripheralData as? [String: Any])
+        } else if peripheral.state == .poweredOff {
+            peripheralManager.stopAdvertising()
+        }
+    }
+    
+    @IBAction func reTransmit(_ sender: Any) {
+        initLocalBeacon()
+    }
+    
+    override func viewDidLoad () {
+        initLocalBeacon()
+    }
 }
 
