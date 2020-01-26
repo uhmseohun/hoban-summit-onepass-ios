d008833233d8a372e51c810e0a70059e5284f7c7
uhmtoto <uhmtotow@gmail.com>
2020-01-26 19:20:07 +0900

    Initial Commit

---
+++ hoban-summit-onepass-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hoban-summit-onepass-ios
+//
+//  Created by Seohun Uhm on 2020/01/26.
+//  Copyright © 2020 uhmtoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hoban-summit-onepass-iosTests/hoban_summit_onepass_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  hoban_summit_onepass_iosTests.swift
+//  hoban-summit-onepass-iosTests
+//
+//  Created by Seohun Uhm on 2020/01/26.
+//  Copyright © 2020 uhmtoto. All rights reserved.
+//
+
+import XCTest
+@testable import hoban_summit_onepass_ios
+
+class hoban_summit_onepass_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
